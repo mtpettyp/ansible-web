01a6bf2b54308392cce1816faa19a0dbe9d03ff4
Mike Pettypiece <mike@pettypiece.ca>
2022-08-22 11:20:42 -0400

    Add support for proxied servers in nginx

--- README.md
+++ README.md
@@ -13,8 +13,10 @@ Role Variables
 * `admin_email` - Email for Let's Encrypt
 * `sites` - List of nginx websites to create
     * `name` - site name
+    * `type` - Type of site to create (static or proxy)
     * `domains` - List of domains for this website
     * `files` - location of website files
+    * `proxied_server` - Server to proxy to (only when type=proxy)
 
 
 Example Playbook
@@ -30,6 +32,13 @@ Example Playbook
           - example.com
           - www.example.com
         files: tests/example.com/html/
+        type: static
+      - name: proxied.example.com
+        domains:
+          - proxied.example.com
+        files: tests/example.com/html/
+        type: proxy
+        proxied_server:  http://127.0.0.1:8009/
   include_role:
     name: "ansible-web"
 ```
--- molecule/default/tests/test_nginx.py
+++ molecule/default/tests/test_nginx.py
@@ -12,7 +12,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 @pytest.mark.parametrize("site", [
     ("example.com")
 ])
-def test_sites(host, site):
-    """Validate that the nginx sites are setup """
+def test_static_sites(host, site):
+    """Validate that the static nginx sites are setup """
     assert host.file(f"/etc/nginx/sites-available/{site}").exists
     assert host.file(f"/var/www/{site}/html/index.html").exists
+    assert host.file(f"/etc/nginx/sites-available/{site}") \
+               .contains("try_files $uri $uri/ =404;")
+
+
+@pytest.mark.parametrize("site", [
+    ("proxied.example.com")
+])
+def test_proxy_sites(host, site):
+    """Validate that the proxied nginx sites are setup """
+    assert host.file(f"/etc/nginx/sites-available/{site}").exists
+    assert host.file(f"/etc/nginx/sites-available/{site}") \
+               .contains("proxy_pass http://127.0.0.1:8009/;")
