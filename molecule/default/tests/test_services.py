af4103cd74826d443c897e0d2f75d91d275408ba
Mike Pettypiece <mike@pettypiece.ca>
2022-08-23 10:08:30 -0400

    Upgrade to Ubuntu 22.04 (jammy)

--- molecule/default/tests/test_services.py
+++ molecule/default/tests/test_services.py
@@ -4,7 +4,7 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('ubuntu-focal-web')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('ubuntu-jammy-web')
 
 """Role testing services using testinfra."""
 
